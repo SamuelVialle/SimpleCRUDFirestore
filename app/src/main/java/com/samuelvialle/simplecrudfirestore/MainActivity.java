0ca65c76a6c5c5c44feea097e295258b52a7ef68
Samuel Vialle <samuelvialle@gmail.com>
2022-11-22 12:11:03 +0100

    Ajout de la vérification avec Alert Dialog

--- app/src/main/java/com/samuelvialle/simplecrudfirestore/MainActivity.java
+++ app/src/main/java/com/samuelvialle/simplecrudfirestore/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -23,36 +24,43 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText etNoteTitle, etNoteContent;
     private Button btnSaveNote, btnShowNote;
+    private TextView tvNoteState;
 
     private FirebaseFirestore db;
 
-    private void initUI(){
+    private void initUI() {
         etNoteTitle = findViewById(R.id.etNoteTitle);
         etNoteContent = findViewById(R.id.etNoteContent);
         btnSaveNote = findViewById(R.id.btnSaveNote);
         btnShowNote = findViewById(R.id.btnShowAllNotes);
+        tvNoteState = findViewById(R.id.tvNoteState);
     }
 
-    private void initFirebaseTools(){
+    private void initFirebaseTools() {
         db = FirebaseFirestore.getInstance();
     }
 
-    private void clicSaveNote(){
+    private void clicSaveNote() {
         btnSaveNote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Récupération des données duformulaire
-                String id = UUID.randomUUID().toString();
                 String title = etNoteTitle.getText().toString();
                 String content = etNoteContent.getText().toString();
-
-                // Apple de la méthode pour la creation en base
-                createDocumentInFirestore(id, title, content);
+                Bundle bundle = getIntent().getExtras();
+                if (bundle != null) {
+                    String id = bundle.getString("uId");
+                    updateDocumentInFirestore(id, title, content);
+                } else {
+                    // Récupération des données duformulaire
+                    String id = UUID.randomUUID().toString();
+                    // Apple de la méthode pour la creation en base
+                    createDocumentInFirestore(id, title, content);
+                }
             }
         });
     }
 
-    private void clicShowAllNotes(){
+    private void clicShowAllNotes() {
         btnShowNote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -62,8 +70,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void createDocumentInFirestore(String id, String title, String content){
-        if(!title.isEmpty() && !content.isEmpty()){
+    /**
+     * CREATE
+     **/
+    private void createDocumentInFirestore(String id, String title, String content) {
+        if (!title.isEmpty() && !content.isEmpty()) {
             // Création d'un tableau qui contoient les data à envoyer sur Firestore
             HashMap<String, Object> map = new HashMap<>();
             map.put("id", id);
@@ -89,6 +100,53 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * UPDATE
+     **/
+    private void updateDocumentInFirestore(String id, String title, String content) {
+        if (!title.isEmpty() && !content.isEmpty()) {
+            db.collection("Notes").document(id).update("title", title, "content", content)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "Data updated!", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(MainActivity.this, ShowAllNotesActivity.class));
+                            } else {
+                                Toast.makeText(MainActivity.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    private void showDataToUpdateFromTheBundle() {
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            // Changement du texte des widgets
+            btnSaveNote.setText("Update note to Firestore");
+            tvNoteState.setText("Update note");
+            btnShowNote.setVisibility(View.GONE);
+            // Récupération des données via le bundle
+            String uId = bundle.getString("uId");
+            String uTitle = bundle.getString("uTitle");
+            String uContent = bundle.getString("uContent");
+            // Association des données au formulaire
+            etNoteTitle.setText(uTitle);
+            etNoteContent.setText(uContent);
+        } else {
+            btnSaveNote.setText("Save note to Firestore");
+
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -96,6 +154,7 @@ public class MainActivity extends AppCompatActivity {
 
         initUI();
         initFirebaseTools();
+        showDataToUpdateFromTheBundle();
         clicSaveNote();
         clicShowAllNotes();
     }
--- app/src/main/java/com/samuelvialle/simplecrudfirestore/NoteAdapter.java
+++ app/src/main/java/com/samuelvialle/simplecrudfirestore/NoteAdapter.java
@@ -1,12 +1,24 @@
 package com.samuelvialle.simplecrudfirestore;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.w3c.dom.Text;
 
@@ -45,11 +57,86 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder
 
     public class NoteViewHolder extends RecyclerView.ViewHolder {
         TextView title, content;
+        ImageView btnEdit, btnDelete;
         public NoteViewHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.tvShowTitle);
             content = itemView.findViewById(R.id.tvShowContent);
+            btnEdit = itemView.findViewById(R.id.btnEdit);
+            btnDelete = itemView.findViewById(R.id.btnDelete);
+
+            btnEdit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    updateNote(getAdapterPosition());
+                }
+            });
+
+            btnDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showAlertDialog(getAdapterPosition());
+                }
+            });
         }
+    }
+
+    private void showAlertDialog(int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setPositiveButton("DELETE", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                deleteNote(position);
+            }
+        });
+        builder.setNegativeButton("CANCEl", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(activity, "Delete canceled", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setTitle("Delete confirmation");
+        builder.setMessage("Are you sure to delete the note ?");
+        builder.create();
+        builder.show();
+
+    }
+
+    private void updateNote(int position) {
+        NoteModel note = notesList.get(position);
+        // Créer un bundle pour envoyer les infos sur la page de modification
+        Bundle bundle = new Bundle();
+        bundle.putString("uId", note.getId());
+        bundle.putString("uTitle", note.getTitle());
+        bundle.putString("uContent", note.getContent());
+
+        Intent intent = new Intent(activity, MainActivity.class);
+        intent.putExtras(bundle);
+        activity.startActivity(intent);
+    }
+
+    private void deleteNote(int position) {
+        NoteModel note = notesList.get(position);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Notes").document(note.getId()).delete()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            notifyRemoved(position);
+                            Toast.makeText(activity, "Data deleted!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(activity, "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void notifyRemoved(int position){
+        notesList.remove(position);
+        notifyItemRemoved(position);
+        activity.readDataFromFirestore();
     }
 
 
